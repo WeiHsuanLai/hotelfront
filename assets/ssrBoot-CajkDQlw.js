82348c2a9cdbc395fb3e961d23254c334112f589
WeiHsuanLai <142381318+WeiHsuanLai@users.noreply.github.com>
2024-08-27 02:23:36 +0000

    Deploying to gh-pages from @ WeiHsuanLai/hotelfront@4d3f263b765e5cd44f2d74ab6a83e6dfa8b3d8ff 🚀

---
+++ assets/ssrBoot-CajkDQlw.js
@@ -0,0 +1,1 @@
+import{K as t,T as e,L as s,bH as n}from"./index-Dldl11PT.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
