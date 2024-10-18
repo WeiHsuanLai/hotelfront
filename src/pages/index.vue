f713c2a659c68cadda88e1e0508d1c25136212a1
WeiHsuanLai <hsuan29684@gmail.com>
2024-10-18 13:41:03 +0800

    刪除沒用的更新

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,9 +14,7 @@
     <v-container>
       <v-row>
         <!-- 遍歷產品列表並顯示每個產品的卡片 -->
-        <v-col cols="12" md="6" lg="3"
-               v-for="product in products" :key="product._id"
-        >
+        <v-col cols="12" md="6" lg="3" v-for="product in products" :key="product._id">
           <RoomCard v-bind="product"></RoomCard>
         </v-col>
       </v-row>
@@ -27,75 +25,48 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import { definePage } from 'vue-router/auto'
-import { useApi } from '@/composables/axios'
-import { useSnackbar } from 'vuetify-use-dialog'
+  import { ref } from 'vue'
+  import { definePage } from 'vue-router/auto'
+  import { useApi } from '@/composables/axios'
+  import { useSnackbar } from 'vuetify-use-dialog'
 
-// 輪播圖
-import Carousel from './index/carousel.vue'
-// 輪播圖2
-import Carousel2 from './index/carousel2.vue'
+  // 輪播圖
+  import Carousel from './index/carousel.vue'
+  // 輪播圖2
+  import Carousel2 from './index/carousel2.vue'
 
-import Map from './index/map.vue'
-import Footer from './index/footer.vue'
-import RoomCard from '../components/RoomCard.vue'
+  import Map from './index/map.vue'
+  import Footer from './index/footer.vue'
+  import RoomCard from '../components/RoomCard.vue'
 
-definePage({
-  meta: {
-    title: '未來飯店首頁',
-    login: false,
-    admin: false
-  }
-})
+  definePage({
+    meta: {
+      title: '未來飯店首頁',
+      login: false,
+      admin: false
+    }
+  })
 
-const { api } = useApi()
-const products = ref([])
-const loadProducts = async () => {
-  try {
-    const { data } = await api.get('/product', {
-      params: {
-        itemsPerPage: 100 // 假設你想一次取出所有產品
-      }
-    })
-    products.value = data.result.data // 直接賦值所有產品
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-loadProducts()
-
-// 新增每15分鐘自動發送請求的函數
-const sendRequestInterval = () => {
-  const intervalId = setInterval(async () => {
+  const { api } = useApi()
+  const products = ref([])
+  const loadProducts = async () => {
     try {
       const { data } = await api.get('/product', {
         params: {
-          itemsPerPage: 100
+          itemsPerPage: 100 // 假設你想一次取出所有產品
         }
       })
-      products.value = data.result.data
-      console.log('成功更新產品列表')
+      products.value = data.result.data // 直接賦值所有產品
     } catch (error) {
-      console.error('更新產品列表失敗:', error)
+      console.error(error)
     }
-  }, 900000) // 每15分鐘（900000毫秒）執行一次
+  }
 
-  return intervalId
-}
-
-// 啟動間隔請求
-const intervalId = sendRequestInterval()
-
-// 在組件銷毀時停止間隔請求
-onUnmounted(() => {
-  clearInterval(intervalId)
-})
+  loadProducts()
 </script>
 
 <style>
-.page-background {
-  background-color: #c1b5b5;
-}
+  .page-background {
+    background-color: #c1b5b5;
+  }
 </style>
